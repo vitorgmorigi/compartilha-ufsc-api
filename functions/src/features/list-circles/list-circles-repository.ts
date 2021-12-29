4cbc5ab2ad4551fd19b0a0c43416eaf523aac45d
Vitor Morigi <vitorgarcezmorigi@gmail.com>
2021-12-29 19:15:00 -0300

    style: 💄 remove unnecessary comments

--- functions/src/features/list-circles/list-circles-repository.ts
+++ functions/src/features/list-circles/list-circles-repository.ts
@@ -3,11 +3,8 @@ import { Circle, CircleDatabase, fromDatabase } from "../../models/circle";
 
 export class ListCirclesRepository {
   constructor(private readonly database: DatabaseService) {}
-  // private db = FirestoreService.getInstance().getCollection("circle");
 
   async list(name?: string): Promise<Circle[]> {
-    // const snapshot = name ? await this.db.where("name_as_array", "array-contains", name).get() : await this.db.get();
-
     const circles = await this.database.searchByNameOrListAll<CircleDatabase>("circle", name);
     const result: Circle[] = [];
     circles.forEach(doc => {
